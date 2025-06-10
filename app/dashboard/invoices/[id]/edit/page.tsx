f5f0883c821bf8abc108ae72b0c8d9396d714514
Vijay <vijayraghav22@gmail.com>
2025-06-10 15:44:54 +0530

    After Server Actions, Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,15 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props:{params: Promise<{id:string}>}) {
   const params = await props.params;
   const id = params.id;
   const [customers,invoice] = await Promise.all([fetchCustomers(),fetchInvoiceById(id)]);
+  if( !invoice){
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -25,11 +25,16 @@ export async function createInvoice(formData: FormData){
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
-
+    
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+            `;
+    } catch(err){
+        console.error(err);
+    }
+    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -42,12 +47,16 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+
+    try{
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+        `;
+    } catch(error){
+        console.error(error);
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
